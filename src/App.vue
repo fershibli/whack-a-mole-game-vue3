0f7ed870440698e1d430fc1132ee4ecdfd720342
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-07-29 16:24:50 -0300

    fix: app template centralization

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="app-container">
     <h1>Whack-a-Mole Game</h1>
     <HelloWorld ref="game" />
     <button @click="startGame" v-if="!gameStarted">Start Game</button>
@@ -26,3 +26,13 @@ export default {
   },
 };
 </script>
+
+<style>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+  margin: 0 auto;
+}
+</style>
